75ac4f1e99e09c7db5b7e23b8924423ae50608b6
etsubeta132 <116604020+etsubeta132@users.noreply.github.com>
2023-02-12 04:15:36 +0300

    Create corpFlightBookings.py

---
+++ corpFlightBookings.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def corpFlightBookings(self, bookings, n):
+        """
+        :type bookings: List[List[int]]
+        :type n: int
+        :rtype: List[int]
+        """
+        ans=[0]*(n+3)
+        for i in bookings:ans[i[0]]+=i[2];ans[i[1]+1]-=i[2]
+        for j in range(1,n+3):ans[j]+=ans[j-1]
+        return ans[1:-2]
