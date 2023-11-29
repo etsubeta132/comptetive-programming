b059f5aaa66425c278ba4aa9763196cdaefae29f
Etsubdink Awoke <116604020+etsubeta132@users.noreply.github.com>
2023-11-30 00:20:55 +0300

    Added solution for Find the Maximum Achievable Number

---
+++ reg-class/find-the-maximum-achievable-number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def theMaximumAchievableX(self, num, t):
+        """
+        :type num: int
+        :type t: int
+        :rtype: int
+        """
+        max_num = (num+t)+t
+        return max_num
