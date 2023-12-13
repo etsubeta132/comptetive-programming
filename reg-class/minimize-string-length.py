17c128ecf6237487845b517cad3e42c1c0379b5e
Etsubdink Awoke <116604020+etsubeta132@users.noreply.github.com>
2023-12-13 22:06:01 +0300

    Added solution for Minimize String Length

--- reg-class/minimize-string-length.py
+++ reg-class/minimize-string-length.py
@@ -1,13 +1,5 @@
 class Solution:
     def minimizedStringLength(self, s: str) -> int:
         
-        str_dict = {}
-        ctr = 0
-
-        for char in s:
-            if char not in str_dict:
-                ctr+=1
-                str_dict[char] =  1
-      
-        return ctr
+        return len(set(s))
         
