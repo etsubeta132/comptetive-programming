b3411c4cf439aec46dd16e8c5d8545395a171b25
Etsubdink Awoke <116604020+etsubeta132@users.noreply.github.com>
2024-01-05 21:55:34 +0300

    Added solution for Sort Colors

--- reg-class/sort-colors.py
+++ reg-class/sort-colors.py
@@ -1,8 +1,26 @@
-class Solution(object):
-    def sortColors(self, nums):
-        for j in range(len(nums)-1):
-            for i in range(len(nums)-1):
-                if nums[i] >= nums[i+1]:
-                    nums[i],nums[i+1] = nums[i+1],nums[i]
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        l,r = 0,0
+        m = len(nums)
+
+        while r < m:
+
+            if nums[r] == 0:
+                nums[l],nums[r] = nums[r],nums[l]
+                l+=1
+            r+=1
+        
+        r = l
+        while r< m:
+            if nums[r] ==1:
+                nums[l],nums[r] = nums[r],nums[l]
+                l+=1
+            r+=1
+
         return nums
-            
+
+        
