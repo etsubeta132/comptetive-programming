c24731f0367bea95d484436e77cf6f3f773b93ea
Etsubdink Awoke <116604020+etsubeta132@users.noreply.github.com>
2024-01-11 17:51:23 +0300

    Add solution for Count Number of Nice Subarrays

---
+++ reg-class/leetcode/count-number-of-nice-subarrays.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def numberOfSubarrays(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        pre_odd_count = {}
+        pre_count = 0
+        res = 0
+        for num in nums:
+            if num % 2 == 1:
+                pre_count += 1
+                pre_odd_count[pre_count] = 1
+            else:
+                pre_odd_count[pre_count] = pre_odd_count.get(pre_count, 0) + 1
+
+            if pre_count == k:
+                res += pre_odd_count.get(0, 0) + 1
+            else:
+                res += pre_odd_count.get(pre_count - k, 0)
+
+        return res
