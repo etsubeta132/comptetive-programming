afd649f8d810c5a0fe91155d4549cecfa9d3cb2a
Etsubdink Awoke <116604020+etsubeta132@users.noreply.github.com>
2024-02-07 22:02:56 +0300

    Add solution for Subarray Sum Equals K

--- reg-class/leetcode/subarray-sum-equals-k.py
+++ reg-class/leetcode/subarray-sum-equals-k.py
@@ -1,21 +1,12 @@
-class Solution(object):
-    def subarraySum(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        result=0
-        current_sum=0
-        sum={0:1}
-
-        for i in nums:
-            current_sum+=i
-            difference=current_sum-k
-
-            result+=sum.get(difference,0)
-            sum[current_sum]=1+sum.get(current_sum,0)
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        prefdict = {0:1}
+        sm = 0
+        ctr = 0
+        for i in range(len(nums)):
+            sm +=nums[i]
+            dif = sm-k
+            ctr += prefdict.get(dif,0)
+            prefdict[sm] = prefdict.get(sm,0)+1
         
-        return result
-
-
+        return ctr
