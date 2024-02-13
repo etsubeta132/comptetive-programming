931daba882bd2f86aa9ed36aafc42f765a223012
Etsubdink Awoke <116604020+etsubeta132@users.noreply.github.com>
2024-02-13 14:52:33 +0300

    Add solution for Reverse Linked List

--- reg-class/leetcode/reverse-linked-list.py
+++ reg-class/leetcode/reverse-linked-list.py
@@ -1,21 +1,23 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def reverseList(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
         if head == None or head.next == None:
             return head
-        previous=None
+
+        previous = None
         current = head
+
         while current:
-            next_node = current.next
+            nextNode = current.next
             current.next = previous
             previous = current
-            current = next_node
+            current = nextNode
+
         return previous
+
+
