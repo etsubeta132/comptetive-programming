1a5491e7c4e2c0984dbd1650dea1e01eaf8608bd
Etsubdink Awoke <116604020+etsubeta132@users.noreply.github.com>
2024-02-18 21:56:25 +0300

    Add solution for Insertion Sort List

---
+++ reg-class/leetcode/leetcode/insertion-sort-list.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def insertionSortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        p = node = ListNode(0)
+        cur = node.next = head
+        while cur and cur.next:
+            val = cur.next.val
+            if cur.val < val:
+                cur = cur.next
+                continue
+            if p.next.val > val:
+                p = node
+            while p.next.val < val:
+                p = p.next
+            new = cur.next
+            cur.next = new.next
+            new.next = p.next
+            p.next = new
+        return node.next
