9cf86cef6ed67058dd51d4d857f98bc55c67e291
Etsubdink Awoke <116604020+etsubeta132@users.noreply.github.com>
2024-02-20 15:02:55 +0300

    Add solution for Power of Two

---
+++ reg-class/leetcode/leetcode/power-of-two.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n < 1:
+            return False
+        
+        if n == 1:
+            return True
+    
+        return n%2==0 and self.isPowerOfTwo(n/2)
+        
+
