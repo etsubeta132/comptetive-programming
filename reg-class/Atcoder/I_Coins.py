5d4f7c1ab99dab46f3e798cb3b8d6ebb5f234083
Etsubdink Awoke <116604020+etsubeta132@users.noreply.github.com>
2024-05-28 14:25:05 +0300

    Create I_Coins.py

---
+++ reg-class/Atcoder/I_Coins.py
@@ -0,0 +1,22 @@
+
+n = int(input())
+probability = list(map(float, input().split()))
+
+dp = [[0]*(n + 1) for j in range(n + 1)]
+
+for i in range((n // 2) + 1, n + 1):
+    dp[i][-1] = 1
+
+for i in range(n):
+    dp[-1][1] = 1
+
+
+for ctr in range(n - 1, -1, -1):
+    for idx in range(n - 1, -1, -1):
+
+        head = probability[idx] * dp[ctr + 1][idx + 1]
+        tail = (1 - probability[idx]) * dp[ctr][idx + 1]
+        
+        dp[ctr][idx] = head + tail
+
+print(dp[0][0])
