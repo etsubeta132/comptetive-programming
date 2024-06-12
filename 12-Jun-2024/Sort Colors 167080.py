d4e54531e95a00a9e82398f6d13d06a9b8eb7c78
Etsubdink Awoke <116604020+etsubeta132@users.noreply.github.com>
2024-06-12 18:21:21 +0300

    Created 'Sort Colors' by A2SV Hub

---
+++ 12-Jun-2024/Sort Colors 167080.py
@@ -0,0 +1,13 @@
+# Problem: Sort Colors - https://leetcode.com/problems/sort-colors/
+
+class Solution(object):
+    def sortColors(self, nums):
+
+        for j in range(len(nums)-1):
+            for i in range(len(nums)-1):
+
+                if nums[i] >= nums[i+1]:
+                    nums[i],nums[i+1] = nums[i+1],nums[i]
+                    
+        return nums
+            
