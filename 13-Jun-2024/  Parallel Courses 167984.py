4283fb63d22ab09c9dc35673442c7d6dbe426089
Etsubdink Awoke <116604020+etsubeta132@users.noreply.github.com>
2024-06-13 22:15:28 +0300

    Created '  Parallel Courses' by A2SV Hub

---
+++ 13-Jun-2024/  Parallel Courses 167984.py
@@ -0,0 +1,44 @@
+# Problem:   Parallel Courses - https://www.codingninjas.com/studio/problems/parallel-courses_1376444
+
+from collections import deque
+
+def parallelCourses(n, prerequisites):
+    
+    indegree = {i:0 for i in range(1, n + 1)}
+    prerequisit = {i:[] for i in range(1, n + 1)}
+    
+    
+    for preq, course in prerequisites:
+        prerequisit[preq].append(course)
+        indegree[course] +=  1
+    
+    semister_count = 0
+    visited = set()
+    queue = deque()
+    
+    for key, val in indegree.items():
+        if val == 0:
+            queue.append(key)
+            visited.add(key)
+    
+    while queue:
+        ln = len(queue)
+        
+        semister_count += 1
+        
+        for i in range(ln):
+            preq = queue.popleft()
+
+            for course in prerequisit[preq]:
+                indegree[course] -= 1
+
+                if indegree[course] == 0 and course not in visited:
+                    queue.append(course)
+                    visited.add(course)
+        
+    
+   
+    if len(visited) == n:
+        return semister_count
+    else:
+        return -1
