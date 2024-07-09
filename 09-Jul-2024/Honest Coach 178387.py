0db418e1f55d813737467a78242225ef2114c6f5
Etsubdink Awoke <116604020+etsubeta132@users.noreply.github.com>
2024-07-10 00:05:10 +0300

    Created 'Honest Coach' by A2SV Hub

---
+++ 09-Jul-2024/Honest Coach 178387.py
@@ -0,0 +1,14 @@
+# Problem: Honest Coach - https://codeforces.com/problemset/problem/1360/B
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    nums = list(map(int, input().split()))
+    nums.sort()
+    ans = float("inf")
+
+    for i in range(1, n):
+        ans = min(ans, nums[i] - nums[i - 1])
+
+    print(ans)
