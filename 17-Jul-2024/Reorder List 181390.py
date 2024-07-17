5fc9951c5ab156430a9cf9554ecc7f10ace2cdf3
Etsubdink Awoke <116604020+etsubeta132@users.noreply.github.com>
2024-07-17 23:27:24 +0300

    Created 'Reorder List' by A2SV Hub

---
+++ 17-Jul-2024/Reorder List 181390.py
@@ -0,0 +1,21 @@
+# Problem: Reorder List - https://leetcode.com/problems/reorder-list/
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        array = []
+        currentlist = head
+        
+        while currentlist is not None:
+            array.append(currentlist)
+            currentlist = currentlist.next
+
+        i = 0
+        halflenth = len(array) // 2
+        while i < halflenth:
+            temp = array[i].next
+            array[i].next = array[~i]
+            array[~i].next = temp
+            i += 1
+        array[halflenth].next = None 
+
+        return head
