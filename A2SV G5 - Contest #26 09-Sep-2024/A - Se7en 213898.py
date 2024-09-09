b5d60382d4f1b61ea7a8370fd0841903aead2f58
Etsubdink Awoke <116604020+etsubeta132@users.noreply.github.com>
2024-09-09 22:27:52 +0300

    Created 'A - Se7en' by A2SV Hub

---
+++ A2SV G5 - Contest #26 09-Sep-2024/A - Se7en 213898.py
@@ -0,0 +1,13 @@
+# Problem: A - Se7en - https://codeforces.com/gym/537362/problem/A
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    if n % 7 == 0:
+        print(n)
+    else:
+        n -= n%10
+        while n%7:
+            n += 1
+        print(n)
