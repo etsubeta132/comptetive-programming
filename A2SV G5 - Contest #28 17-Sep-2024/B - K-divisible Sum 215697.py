f1ffda8107c717dc8bd5fdc2d638ebbb22f1dbe0
Etsubdink Awoke <116604020+etsubeta132@users.noreply.github.com>
2024-09-18 02:10:05 +0300

    Created 'B - K-divisible Sum' by A2SV Hub

---
+++ A2SV G5 - Contest #28 17-Sep-2024/B - K-divisible Sum 215697.py
@@ -0,0 +1,16 @@
+# Problem: B - K-divisible Sum - https://codeforces.com/gym/540354/problem/B
+
+import math
+t = int(input())
+
+for _ in range(t):
+    n, k = map(int, input().split())
+    ans = 0
+    if n % k == 0:
+        ans = (n //  k) * k
+    else:
+        ans = ((n //  k) + 1) * k
+    print(math.ceil(ans / n))
+    
+
+    
