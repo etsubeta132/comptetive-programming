7b546bf3c5ee3305a1beec2abb7c380a7d9852d3
Etsubdink Awoke <116604020+etsubeta132@users.noreply.github.com>
2024-09-27 22:28:51 +0300

    Created 'Longest Subarray With Maximum Bitwise AND' by A2SV Hub

---
+++ 27-Sep-2024/Longest Subarray With Maximum Bitwise AND 216961.py
@@ -0,0 +1,14 @@
+# Problem: Longest Subarray With Maximum Bitwise AND - https://leetcode.com/problems/longest-subarray-with-maximum-bitwise-and/
+
+class Solution(object):
+    def longestSubarray(self, nums):
+        ans, cnt = 0, 0
+        
+        max_element = max(nums)
+        for num in nums:
+            if num == max_element:
+                cnt += 1
+            else:
+                cnt = 0
+            ans = max(ans, cnt)
+        return ans
