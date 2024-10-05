659baa7d66fed979320ae5e4a703415ed7fb0c48
Etsubdink Awoke <116604020+etsubeta132@users.noreply.github.com>
2024-10-05 21:18:32 +0300

    Created 'My Calendar II' by A2SV Hub

---
+++ 05-Oct-2024/My Calendar II 218279.py
@@ -0,0 +1,18 @@
+# Problem: My Calendar II - https://leetcode.com/problems/my-calendar-ii/
+
+class MyCalendarTwo:
+    def __init__(self):
+        self.mp = defaultdict(int)
+        
+    def book(self, start: int, end: int) -> bool:
+        self.mp[start] += 1
+        self.mp[end] -= 1
+
+        ActiveCount = 0
+        for value, count in sorted(self.mp.items()):
+            ActiveCount += count
+            if ActiveCount > 2:
+                self.mp[start] -= 1
+                self.mp[end] += 1
+                return False
+        return True
